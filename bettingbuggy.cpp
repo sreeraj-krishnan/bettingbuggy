dce9b6d83714208e402747daca3f787d8bf703fd
root <root@vmi202821.contaboserver.net>
2018-09-29 11:23:27 +0200

    improvements

--- bettingbuggy.cpp
+++ bettingbuggy.cpp
@@ -40,14 +40,34 @@ class bettingbuggy : public eosio::contract {
 	  accountsg(_self, _self)       
 	{}
 
+	
+	// @abi action
+	void withdraw(account_name to, asset quantity)
+	{
+		require_auth(to);
+                eosio_assert( quantity.is_valid(), "invalid quantity" );
+                eosio_assert( quantity.amount > 0, "amount > 0" );
+		
+		auto itr = accountsg.find(to);
+		eosio_assert( itr != accountsg.end(), "cannot find account");
+		eosio_assert( itr->eos_balance.amount >= quantity.amount , "not sufficient balance");
+		
+		action(
+                       	permission_level{ _self, N(active) },
+                       	N(eosio.token), N(transfer),
+                       	std::make_tuple(_self, to, quantity , std::string("amount successfully withdrawn!"))
+                ).send();
 
-     	 // @abi action
-      	void betrandom(account_name user, asset quantity , string guess)
+			
+	}
+
+	// @abi action
+	void deposit(account_name user, asset quantity)
 	{
 		require_auth(user);
                 eosio_assert( quantity.is_valid(), "invalid quantity" );
                 eosio_assert( quantity.amount > 0, "amount > 0" );
-                eosio_assert( quantity.amount <= 3, "amount <= 3" );
+		
 		auto itr = accountsg.find(user);
          	if( itr == accountsg.end() )
 	       	{
@@ -60,27 +80,61 @@ class bettingbuggy : public eosio::contract {
                                 N(eosio.token), N(transfer),
                                 std::make_tuple(user, _self, quantity, std::string("betting stakes from user"))
                               ).send();
-	
-		accountsg.modify( itr, 0, [&]( auto& acnt ) {
+		
+		 accountsg.modify( itr, 0, [&]( auto& acnt ) {
             		acnt.eos_balance += quantity;
          	});	
+			
+	}
+		
+
+     	 // @abi action
+      	void betrandom(account_name user, asset quantity , string guess)
+	{
+		require_auth(user);
+                eosio_assert( quantity.is_valid(), "invalid quantity" );
+                eosio_assert( quantity.amount > 0, "amount > 0" );
+		
+		auto itr = accountsg.find(user);
+
+		eosio_assert( itr != accountsg.end() , "user has no deposit");
+		eosio_assert( itr->eos_balance.amount >= quantity.amount , "insufficient balance to bet");
+		
+		// staked amount needs to be deducted from balance
+		accountsg.modify( itr, 0, [&]( auto& acnt ) {
+            			acnt.eos_balance -= quantity;
+	        });
+
 		uint32_t random_value=  get_random_number(user,10);
 		
 		if( atoi(guess.c_str()) == random_value)
 		{
-			asset tquantity = quantity;
-			tquantity.amount = 2;
+			// deposit the winnings
+			accountsg.modify( itr, 0, [&]( auto& acnt ) {
+        	    		acnt.eos_balance += 2*quantity;
+	        	});
+		}
+        }
+     	 
+	// @abi action
+      	void buggybet(account_name user, asset quantity , string guess)
+	{
+		require_auth(user);
+		
+		uint32_t random_value=  get_random_number(user,10);
+		
+		if( atoi(guess.c_str()) == random_value)
+		{
 			action(
                         	permission_level{ _self, N(active) },
                         	N(eosio.token), N(transfer),
-                        	std::make_tuple(_self,user, tquantity , std::string("you win double the amount back !!"))
+                        	std::make_tuple(_self,user, quantity*2 , std::string("You win double the staked EOS !!"))
                         ).send();
-			accountsg.modify( itr, 0, [&]( auto& acnt ) {
-            			acnt.eos_balance -= tquantity;
-         		});	
 
 		}
         }
+
+
   private:
 	 
 	 //@abi table accountsb i64
@@ -101,4 +155,4 @@ class bettingbuggy : public eosio::contract {
        account_index     accountsg;
 };
 
-EOSIO_ABI( bettingbuggy, (betrandom) )
+EOSIO_ABI( bettingbuggy, (betrandom)(deposit)(withdraw)(buggybet) )
